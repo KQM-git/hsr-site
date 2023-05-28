fa985b54d1fcb78dfa9c05624713c7a8b5a44186
neia <51728998+neeia@users.noreply.github.com>
2023-05-27 22:00:36 -0700

    Update rarity to use unicode stars instead of mask

---
+++ src/styles/dynamic/rarity-colors.ts
@@ -0,0 +1,5 @@
+// WARNING: Do not replace with string interpolation.
+// See: https://tailwindcss.com/docs/content-configuration#dynamic-class-names
+const rarities = ['', 'text-stars-1', 'text-stars-2', 'text-stars-3', 'text-stars-4', 'text-stars-5'];
+
+export default rarities;
