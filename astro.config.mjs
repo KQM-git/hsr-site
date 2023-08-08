5a9895b0c5863d4761fa7ff16b799c2c7337c476
neia <51728998+neeia@users.noreply.github.com>
2023-08-08 16:54:21 -0700

    Add preact and radix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import mdx from "@astrojs/mdx";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind(), preact({ compat: true })],
   trailingSlash: 'never',
   build: {
     format: "file"
-  }
+  },
 });
